40c34cf2d9d3ae81fa1239e96e8f51c12a03e50d
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-20 20:11:17 -0300

    feat: create ProfileCard component

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  faBuilding,
+  faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "../Link";
+import * as S from "./styles";
+
+export function ProfileCard() {
+  return (
+    <S.ProfileCardContainer>
+      <img src="https://github.com/kevenpacheco.png" height="148" width="148" />
+
+      <div>
+        <header>
+          <h2>Cameron Williamson</h2>
+
+          <Link>
+            github <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </Link>
+        </header>
+
+        <p>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </p>
+
+        <footer>
+          <S.GithubContainer>
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+            cameronwll
+          </S.GithubContainer>
+
+          <S.CompanyContainer>
+            <FontAwesomeIcon icon={faBuilding} />
+            Rocketseat
+          </S.CompanyContainer>
+
+          <S.FollowersContainer>
+            <FontAwesomeIcon icon={faUserGroup} />
+            32 seguidores
+          </S.FollowersContainer>
+        </footer>
+      </div>
+    </S.ProfileCardContainer>
+  );
+}
---
+++ src/components/ProfileCard/styles.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+export const ProfileCardContainer = styled.section`
+  background: ${({ theme }) => theme.colors.base.profile};
+  box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  padding: 2rem;
+  padding-left: 2.5rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  img {
+    width: 9.25rem;
+    height: 9.25rem;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1.5rem;
+
+    h2 {
+      line-height: 130%;
+    }
+  }
+
+  p {
+    margin-top: .5rem;
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+    margin-top: 1.5rem;
+  }
+`;
+
+const SocialContainer = styled.span`
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+  color: ${({ theme }) => theme.colors.base.subtitle};
+
+  svg {
+    color: ${({ theme }) => theme.colors.base.label};
+  }
+`
+
+export const GithubContainer = styled(SocialContainer)`
+  svg {
+    font-size: 1.055rem;
+  }
+`;
+
+export const CompanyContainer = styled(SocialContainer)`
+  svg {
+    font-size: 1.055rem;
+  }
+`;
+
+export const FollowersContainer = styled(SocialContainer)`
+  svg {
+    font-size: .8438rem;
+  }
+`;
