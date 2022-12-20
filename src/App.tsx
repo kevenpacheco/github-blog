d9b14a051686919e9ddbe12dcca7f700dfe7281c
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-19 22:48:40 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
 import { DefaultTheme } from "./styles/themes/defaultTheme";
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyles />
       <div className="App">
       </div>
     </ThemeProvider>
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({theme}) => theme.colors.blue}
+  }
+
+  body {
+    background-color: ${({theme}) => theme.colors.base.background};
+    color: ${({theme}) => theme.colors.base.text};
+    --webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Nunito', sans-serif; 
+  }
+`;
