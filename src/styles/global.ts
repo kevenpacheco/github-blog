73da1f54b05c62fb53a9a2248e7a3395c6bc08a8
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-19 23:38:36 -0300

    feat (global styles): add default line height

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,16 +9,17 @@ export const GlobalStyles = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${({theme}) => theme.colors.blue}
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.blue}
   }
 
   body {
-    background-color: ${({theme}) => theme.colors.base.background};
-    color: ${({theme}) => theme.colors.base.text};
+    background-color: ${({ theme }) => theme.colors.base.background};
+    color: ${({ theme }) => theme.colors.base.text};
     --webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
-    font: 400 1rem 'Nunito', sans-serif; 
+    font: 400 1rem 'Nunito', sans-serif;
+    line-height: 160%;
   }
 `;
