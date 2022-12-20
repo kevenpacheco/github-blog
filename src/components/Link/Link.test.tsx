944bd98a75e5746cc0bc1fbed2431bcaa26f6f9e
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-20 20:22:22 -0300

    test: add theme provider

--- src/components/Link/Link.test.tsx
+++ src/components/Link/Link.test.tsx
@@ -1,15 +1,21 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import { Link } from '.'
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Link } from ".";
+import { ThemeProvider } from "styled-components";
+import { DefaultTheme } from "../../styles/themes/defaultTheme";
 
-describe('<Link />', () => {
-  it('should render Link component', () => {
-    const linkText = 'Texto do link'
+describe("<Link />", () => {
+  it("should render Link component", () => {
+    const linkText = "Texto do link";
 
-    render(<Link>{linkText}</Link>);
+    render(
+      <ThemeProvider theme={DefaultTheme}>
+        <Link>{linkText}</Link>
+      </ThemeProvider>
+    );
 
-    const sut = screen.getByRole('link', {name: linkText})
+    const sut = screen.getByRole("link", { name: linkText });
 
-    expect(sut).toBeInTheDocument()
-  })
-})
+    expect(sut).toBeInTheDocument();
+  });
+});
