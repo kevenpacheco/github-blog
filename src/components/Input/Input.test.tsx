428f7097dd98bca0eef333e094e94b9d7ec3dd78
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-20 23:31:11 -0300

    test (PublicationCard component): add theme provider

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,13 +1,19 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import { Input } from '.'
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Input } from ".";
+import { ThemeProvider } from "styled-components";
+import { DefaultTheme } from "../../styles/themes/defaultTheme";
 
-describe('<Input />', () => {
-  it('should render Input component', () => {
-    render(<Input />);
+describe("<Input />", () => {
+  it("should render Input component", () => {
+    render(
+      <ThemeProvider theme={DefaultTheme}>
+        <Input />
+      </ThemeProvider>
+    );
 
-    const sut = screen.getByPlaceholderText("Buscar conteúdo")
+    const sut = screen.getByPlaceholderText("Buscar conteúdo");
 
-    expect(sut).toBeInTheDocument()
-  })
-})
+    expect(sut).toBeInTheDocument();
+  });
+});
