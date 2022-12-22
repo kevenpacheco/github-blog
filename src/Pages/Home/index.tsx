eb27daea5f93031a66c17f345912a4094a94b214
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-22 14:07:33 -0300

    feat: fetch all publications from this repository on github

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Input } from "../../components/Input";
 import { ProfileCard } from "../../components/ProfileCard";
 import { PublicationCard } from "../../components/PublicationCard";
 import { BaseLayout } from "../../layouts/BaseLayout";
+import { api } from "../../service/api";
 import * as S from "./styles";
 
+interface PublicationType {
+  id: number;
+}
+
 export function Home() {
+  const [publications, setPublications] = useState<PublicationType[]>([]);
+  const [publicationCount, setPublicationCount] = useState(0);
+
+  useEffect(() => {
+    const repo = "kevenpacheco/github-blog";
+
+    api.get(`/search/issues?q=repo:${repo}`).then((response) => {
+      setPublications(response.data.items);
+      setPublicationCount(response.data.total_count);
+    });
+  }, []);
+
   return (
     <BaseLayout>
       <ProfileCard />
@@ -14,17 +32,16 @@ export function Home() {
           <header>
             <span>Publicações</span>
 
-            <span>6 publicações</span>
+            <span>{publicationCount} publicações</span>
           </header>
 
           <Input />
         </S.SearchContainer>
 
         <S.PublicationsContainer>
-          <PublicationCard />
-          <PublicationCard />
-          <PublicationCard />
-          <PublicationCard />
+          {publications.map((publication) => (
+            <PublicationCard key={publication.id} />
+          ))}
         </S.PublicationsContainer>
       </S.HomeContainer>
     </BaseLayout>
