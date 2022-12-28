9bc4f412faa6b6f56bf62c35538641fd421d8318
Keven Pacheco <kevenpacheco2000@gmail.com>
2022-12-28 19:45:11 -0300

    test (PublicationCard component): adjust tests

--- src/components/PublicationCard/PublicationCard.test.tsx
+++ src/components/PublicationCard/PublicationCard.test.tsx
@@ -3,16 +3,27 @@ import { render, screen } from "@testing-library/react";
 import { PublicationCard } from ".";
 import { ThemeProvider } from "styled-components";
 import { DefaultTheme } from "../../styles/themes/defaultTheme";
+import { BrowserRouter } from "react-router-dom";
 
 describe("<PublicationCard />", () => {
   it("should render PublicationCard component", () => {
+    const fakePublicationCardData = {
+      id: 1,
+      title: "Titulo da publicação fake",
+      body: "Conteúdo da publicação fake",
+      number: 1,
+      created_at: "2022-12-28T22:37:29.585Z",
+    };
+
     render(
       <ThemeProvider theme={DefaultTheme}>
-        <PublicationCard/> 
+        <BrowserRouter>
+          <PublicationCard data={fakePublicationCardData} />
+        </BrowserRouter>
       </ThemeProvider>
     );
 
-    const sut = screen.getByText(/JavaScript data types and data structures/i);
+    const sut = screen.getByText(/Titulo da publicação fake/i);
 
     expect(sut).toBeInTheDocument();
   });
